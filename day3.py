d405c1f9c454d5ca881f7e727ed3e47a2c2696df
Jordy de Wit <jordy1998@live.nl>
2025-12-11 02:51:46 +0100

    Added day 3 and day 4 part 1

---
+++ day3.py
@@ -0,0 +1,58 @@
+import os
+from itertools import combinations
+import time
+
+def puzzle_one(data):
+    # result = 0
+    return sum([max(int(''.join(i)) for i in combinations(battery_bank.strip(), 2)) for battery_bank in data])
+    # for battery_bank in data:
+    #     values = battery_bank.strip()
+    #     max_val = max(int(''.join(i)) for i in combinations(values, 2))
+    #     print(f"Max value for bank {values} is {max_val}")
+    #     result += max_val
+    # return result
+
+def puzzle_two(data):
+    # Combinations of 12 from each battery bank is way too slow when each bank has 100 digits.
+    # return sum([max(int(''.join(i)) for i in combinations(battery_bank.strip(), 12)) for battery_bank in data])
+    ### vvvv I peeked at the solution vvvv ###
+    result = 0
+    for battery_bank in data:
+        res_jolt = list()
+        start_idx = 0
+        battery_bank = battery_bank.strip()
+        for _ in range(12):
+            can_skip = len(battery_bank) - start_idx - (12 - len(res_jolt))
+            search_space = battery_bank[start_idx:start_idx + can_skip + 1]
+            max_jolt = max(search_space)
+            best_pos = start_idx + search_space.index(max_jolt)
+            res_jolt.append(max_jolt)
+            start_idx = best_pos + 1
+        result += int(''.join(res_jolt))
+
+    return result
+    
+
+with open(os.path.join("data_files", "day_3", "example.txt")) as f:
+    data = f.readlines()
+    start = time.time()
+    print(f"Example puzzle one: {puzzle_one(data)}")
+    print(f"Took {time.time() - start} seconds")
+
+with open(os.path.join("data_files", "day_3", "puzzle_1.txt")) as f:
+    data = f.readlines()
+    start = time.time()
+    print(f"Puzzle one: {puzzle_one(data)}")
+    print(f"Took {time.time() - start} seconds")
+
+with open(os.path.join("data_files", "day_3", "example.txt")) as f:
+    data = f.readlines()
+    start = time.time()
+    print(f"Example puzzle two: {puzzle_two(data)}")
+    print(f"Took {time.time() - start} seconds")
+
+with open(os.path.join("data_files", "day_3", "puzzle_2.txt")) as f:
+    data = f.readlines()
+    start = time.time()
+    print(f"Puzzle two: {puzzle_two(data)}")
+    print(f"Took {time.time() - start} seconds")
---
+++ day4.py
@@ -0,0 +1,53 @@
+import os
+import time
+
+def puzzle_one(data):
+    x_size = len(data[0])
+    y_size = len(data)
+    result = 0
+    # papers = []
+    for y, data_row in enumerate(data):
+        for x in range(x_size):
+            if data_row[x] == '@':
+                papers_seen = -1
+                nine_by_nine = list()
+                if y > 0:
+                    nine_by_nine.append(data[y-1][max(0, x-1):min(x_size, x+2)])
+                nine_by_nine.append(data_row[max(0, x-1):min(x_size, x+2)])
+                if y < y_size - 1:
+                    nine_by_nine.append(data[y+1][max(0, x-1):min(x_size, x+2)])
+                papers_seen += sum(nine_by_nine.count('@') for nine_by_nine in nine_by_nine)
+                if papers_seen < 4:
+                    result += 1
+                    # papers.append((y, x))
+    # for x,y in papers:
+    #     data[x] = data[x][:y] + 'x' + data[x][y+1:]
+    # print('\n'.join(''.join(row) for row in data))
+    return result
+              
+def puzzle_two(data):
+    pass
+
+with open(os.path.join("data_files", "day_4", "example.txt")) as f:
+    data = [x.strip() for x in f.readlines()]
+    start = time.time()
+    print(f"Example puzzle one: {puzzle_one(data)}")
+    print(f"Took {time.time() - start} seconds")
+
+with open(os.path.join("data_files", "day_4", "puzzle_1.txt")) as f:
+    data = [x.strip() for x in f.readlines()]
+    start = time.time()
+    print(f"Puzzle one: {puzzle_one(data)}")
+    print(f"Took {time.time() - start} seconds")
+
+with open(os.path.join("data_files", "day_4", "example.txt")) as f:
+    data = [x.strip() for x in f.readlines()]
+    start = time.time()
+    print(f"Example puzzle two: {puzzle_two(data)}")
+    print(f"Took {time.time() - start} seconds")
+
+with open(os.path.join("data_files", "day_4", "puzzle_2.txt")) as f:
+    data = [x.strip() for x in f.readlines()]
+    start = time.time()
+    print(f"Puzzle two: {puzzle_two(data)}")
+    print(f"Took {time.time() - start} seconds")
